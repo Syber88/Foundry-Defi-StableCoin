f86d9c9d7e974dd0024489eef4fbd36af9ec9ce1
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-24 21:50:22 +0200

    completed deposit collateral

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {DecentralisedStableCoin} from "./DecentralisedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title DSCEngine
@@ -15,24 +17,33 @@ import {DecentralisedStableCoin} from "./DecentralisedStableCoin.sol";
  *
  * Our DSC should always be "overCollateralised". At no point should the value of all the collateral be less than or equal to the value of all the DSC.
  */
-contract DSCEngine {
+contract DSCEngine is ReentrancyGaurd {
     //////////////////
     // Errors       //
     //////////////////
     error DSCEngine__NeedsMoreThanZero();
     error DSCEngine__TokenAdressesAndPriceFeedAddressMustBeSameLength();
     error DSCEngine__TokenNotAllowed();
+    error DSCEngine__TransferFailed(); 
 
     //////////////////////
     // State Variables  //
     //////////////////////
 
     mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
     DecentralisedStableCoin private immutable i_dsc;
+
+    //////////////
+    // Events  //
+    /////////////
+
+    event CollateraDeposited(address indexed msg.sender, address indexed tokenCollateralAddress, uint256 indexed amountCollateral);
 
     //////////////////
     // Modifiers    //
     //////////////////
+
     modifier moreThanZero(uint256 amount) {
         if (amount == 0) {
             revert DSCEngine___NeedsMoreThanZero();
@@ -65,7 +76,9 @@ contract DSCEngine {
     /////////////////////////
     // External Functions  //
     ////////////////////////
+
     function depositCollateralAndMintDsc() external {}
+
     /**
      *
      * @param tokenCollateralAddress The address of the token to deposit as collateral
@@ -75,7 +88,16 @@ contract DSCEngine {
         external
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
-    {}
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateraDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral)
+        if (!success){
+            revert DSCEngine__TransferFailed();
+        }
+
+    }
 
     function redeemCollateralForDsc() external {}
     function redeemCollateral() external {}
