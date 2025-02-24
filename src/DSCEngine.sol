1773c9d4cbbc0cc925c9caaa10bd42419d078f2c
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-24 21:22:26 +0200

    format and added functionality that wasnt being saved befvore

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {DecentralisedStableCoin} from "./DecentralisedStableCoin.sol";
+
 /**
  * @title DSCEngine
  * @author 0xsyber88
@@ -14,67 +16,66 @@ pragma solidity ^0.8.24;
  * Our DSC should always be "overCollateralised". At no point should the value of all the collateral be less than or equal to the value of all the DSC.
  */
 contract DSCEngine {
-    
     //////////////////
     // Errors       //
     //////////////////
     error DSCEngine__NeedsMoreThanZero();
-    error DSCEngine__Token
+    error DSCEngine__TokenAdressesAndPriceFeedAddressMustBeSameLength();
+    error DSCEngine__TokenNotAllowed();
 
     //////////////////////
     // State Variables  //
     //////////////////////
-    
 
-     mapping (address token => address pricesFeed) s_priceFeeds;
-
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    DecentralisedStableCoin private immutable i_dsc;
 
     //////////////////
     // Modifiers    //
     //////////////////
-    modifier moreThanZero (uint256 amount) {
-        if (amount ==0){
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
             revert DSCEngine___NeedsMoreThanZero();
         }
         _;
-    
     }
 
-    // modifier isAllowedToken(){}
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__TokenNotAllowed();
+        }
+        _;
+    }
 
     //////////////////
     // Functions    //
     //////////////////
 
-    constructor(
-        address[] memory tokenAddress,
-        address[] memory priceFeedAddress,
-        address dscAddress
-    ){
-        if (tokenAddress.length != priceFeedAddress.length) {
-            revert();
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAdressesAndPriceFeedAddressMustBeSameLength();
         }
 
-
-
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralisedStableCoin(dscAddress);
     }
 
     /////////////////////////
     // External Functions  //
     ////////////////////////
-    function depositCollateralAndMintDsc() external {
-
-
-    }
+    function depositCollateralAndMintDsc() external {}
     /**
-     * 
+     *
      * @param tokenCollateralAddress The address of the token to deposit as collateral
      * @param amountCollateral Amount of collateral to deposit
      */
-    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral) external moreThanZero(amountCollateral){
-
-
-    }
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+    {}
 
     function redeemCollateralForDsc() external {}
     function redeemCollateral() external {}
