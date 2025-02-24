09cb83f1802bdef3509006eb70bc0c8a60a32a8c
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-24 12:32:44 +0200

    added mint implementation

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -12,14 +12,14 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * Relative Stability: Pegged to USD
  * This Contract is meant to be governed by DSCEngine. THis contract is just the ERC20 implementation of our stablecoin system
  */
-contract DecentralisedStableCoin is ERC20Burnable, Ownable{
-
+contract DecentralisedStableCoin is ERC20Burnable, Ownable {
     error DecentralisedStableCoin__MustBeGreaterThanZero();
     error DecentralisedStableCoin__BurnAmountMustExceedBalance();
+    error DecentralisedStableCoin__NotZeroAddress();
 
     constructor() ERC20("DecentralisedStableCoin", "DSC") Ownable(msg.sender) {}
 
-    function burn(uint256 _amount) public override onlyOwner{
+    function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (_amount <= 0) {
             revert DecentralisedStableCoin__MustBeGreaterThanZero();
@@ -28,8 +28,17 @@ contract DecentralisedStableCoin is ERC20Burnable, Ownable{
             revert DecentralisedStableCoin__BurnAmountMustExceedBalance();
         }
         super.burn(_amount);
-
-
     }
 
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralisedStableCoin__NotZeroAddress();
+        }
+
+        if (_amount <= 0) {
+            revert DecentralisedStableCoin__MustBeGreaterThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
