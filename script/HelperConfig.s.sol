49a60b318d8d216950d62955c12a0e14641a2145
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-25 23:44:36 +0200

    partially done with configuring anvil

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregatorff} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 
 contract HelperConfig is Script{
@@ -13,6 +15,10 @@ contract HelperConfig is Script{
         address wbtc;
         uint256 deployerKey;
     }
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
 
     NetworkConfig public activeNetworkConfig;
 
@@ -30,6 +36,19 @@ contract HelperConfig is Script{
         });
     }
 
-    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory)
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory){
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)){
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            ETH_USD_PRICE
+            
+        );
+        ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+        vm.stopBroadcast();
+    }
 
 }
