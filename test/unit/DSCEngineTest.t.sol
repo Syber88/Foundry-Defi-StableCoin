f0391be8d820bae748f88587c52e00ba609dcb0d
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-26 10:38:50 +0200

    initialised engine test

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralisedStableCoin} from "../../src/DecentralisedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralisedStableCoin dsc;
+    DSCEngine dscEngine;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        vm.startBroadcast();
+        (dsc, dscEngine) = deployer.run();
+        vm.stopBroadcast();
+
+    }
+
+}
