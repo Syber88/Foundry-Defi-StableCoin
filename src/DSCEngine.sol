72657665bbde9b2ddead158811ec685678236651
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-03-02 15:02:32 +0200

    added pricefeed stale price checks

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -5,6 +5,7 @@ import {DecentralisedStableCoin} from "./DecentralisedStableCoin.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {OracleLib} from "./OracleLib.sol";
 
 /**
  * @title DSCEngine
@@ -31,6 +32,11 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__MintFailed();
     error DSCEngine__HealthFactorOkay();
     error DSCEngine__HealthFactorNotImproved();
+    //////////////////
+    // Types        //
+    //////////////////
+
+    using OracleLib for AggregatorV3Interface;
 
     //////////////////////
     // State Variables  //
@@ -264,7 +270,7 @@ contract DSCEngine is ReentrancyGuard {
 
     function getTokenAmountFromUsd(address token, uint256 usdAmountInWei) public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
-        (, int256 price,,,) = priceFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
         return (usdAmountInWei * PRECISION) / (uint256(price) * FEED_PRECISION);
     }
 
---
+++ src/OracleLib.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+/**
+ * @title OracleLibe
+ * @author 0xsyber88
+ * @notice Thit library is used to check the Chainlink oracle for stale data. if price is stale, the
+ *  function will rever and render the DSCEngine unusable
+ */
+library OracleLib {
+    error OracleLib__StalePrice();
+
+    uint256 private constant TIMEOUT = 3 hours;
+
+    function staleCheckLatestRoundData(AggregatorV3Interface priceFeed)
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+            priceFeed.latestRoundData();
+
+        uint256 secondsSince = block.timestamp - updatedAt;
+        if (secondsSince > TIMEOUT) revert OracleLib__StalePrice();
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
+    }
+}
